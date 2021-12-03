782873a027bc7e3189fdab98b31254667aeddddc
Aleksandr846 <72504562+Aleksandr846@users.noreply.github.com>
2021-12-03 23:46:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Лабораторная работа 9</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <form id="frm" method="POST" action="">
+        <p>Введите число между -1 и 1:</p>
+        <input type="text" name="x">
+        <p>Введите n:</p>
+        <input type="text" name="n">
+        <input type="submit" value="OK">
+    </form>
+    <?php
+        $x=$_POST["x"];
+        $n=$_POST["n"];
+        for($a=1; $a<=$n; $a++){
+            $b=$b+(($x**$a)/$a);
+        }
+        $b=$b*-1;
+        echo $b;
+    ?>
+</body>
+</html>
